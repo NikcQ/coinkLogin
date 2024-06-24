0478e7313a7c9fa4d9295230e6c0d5528343898d
Nicolas La Rotta <56049059+NikcQ@users.noreply.github.com>
2024-06-24 17:44:11 -0500

    fix(coinkApp): fixed doubled inputs (#4)
    * chore(coink): finished pages and started routing
    
    * chore(coinkApp): started validation, almost there
    
    * chore(coinkApp): improved validation
    
    * chore(coinkApp): validated telephone number
    
    * fix(coinkApp): merge conflict duplicated imports
    
    ---------
    
    Co-authored-by: Nicolas La Rotta <nicolas.larotta@amadeus.com>

--- coinkApp/src/app/landing/landing.page.ts
+++ coinkApp/src/app/landing/landing.page.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-landing',
@@ -9,7 +9,7 @@ import { Router } from '@angular/router';
 })
 export class LandingPage implements OnInit {
   constructor(private router: Router) {}
-  constructor(private router: Router) {}
+
 
   ngOnInit() {}
 
